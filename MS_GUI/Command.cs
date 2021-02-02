6e2f7b2c659a69afb069fcd0e2a0ae18d4af186d
Josh Blades <joshblades@outlook.com>
2021-02-02 20:16:38 +0000

    Add project files.

---
+++ MS_GUI/Command.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+namespace MS_GUI
+{
+    class Command
+    {
+        // Create a new instance of the server
+        public Process server = new Process();
+
+        // Instance of the command class
+        public Command() { }
+
+        //private int RamAmount = 0;
+
+        // Holds the players currently online
+        List<string> Users = new List<string>();
+
+        /// <summary>
+        /// Create the CMD server 
+        /// </summary>
+        public async Task InitialiseCMD(string args)
+        {
+            await Task.Run(() =>
+            {
+                // Tell it we want a command line
+                server.StartInfo.FileName = "CMD.exe";
+
+                // Give the args - mainly how much RAM you want
+                server.StartInfo.Arguments = "/c " + args;
+
+                // Get the ram amount
+                //RamAmount = int.Parse(args[9..^33]);
+
+                // So the user doesn't see the CMD - FIND A WAY TO KILL THIS PROCESS WHEN THE APP ENDS
+                server.StartInfo.CreateNoWindow = true;
+
+                // Use pure CMD
+                server.StartInfo.UseShellExecute = false;
+
+                // Allows commands to be retrieved from the server
+                server.StartInfo.RedirectStandardOutput = true;
+                server.StartInfo.RedirectStandardError = true;
+
+                // Allows commands to be sent to the server
+                server.StartInfo.RedirectStandardInput = true;
+
+                // Get the ouput data and send it to the user
+                server.OutputDataReceived += new DataReceivedEventHandler(GetServerOutput);
+
+                // server.ErrorDataReceived += (s, e) => mainWindow.InfoTextBox.Text = e.Data;
+
+                // Start the server
+                server.Start();
+                server.BeginOutputReadLine();
+                ProcStats();
+                //server.BeginErrorReadLine();
+                server.WaitForExit();
+            });
+        }
+
+        /// <summary>
+        /// Commands to be sent to the CMD from the user
+        /// </summary>
+        /// <param name="inCommand"></param>
+        public void SendCommand(string inCommand)
+        {
+            try
+            {
+                // Check to see if there is a command
+                if (inCommand != "")
+                    server.StandardInput.WriteLine(inCommand);
+            }
+            catch
+            {
+                MessageBox.Show("Unable to send command!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Gets server information to update GUI
+        /// </summary>
+        public void GetServerOutput(Object sendingProcess, DataReceivedEventArgs outInfo)
+        {
+            if (outInfo.Data == null)
+                Environment.Exit(0);
+
+            // Check if players join or leave 
+            if (outInfo.Data.Contains('>'))
+            {
+                string userName = outInfo.Data.Split('<', '>')[1].Trim();
+                string message = outInfo.Data.Split('>')[1].Trim();
+
+                // Secret danny commands shhh
+                if (userName == "Dauttu" && message.StartsWith(@"\"))
+                {
+                    SendCommand(message.Remove(0, 1));
+                }
+            }
+            // Add players online
+            else if (outInfo.Data.Contains("joined the game"))
+            {
+                string userName = outInfo.Data[32..^15].Trim();
+
+                // Invoke the UI thread 
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+
+                    // Adds the user online to the list
+                    string onlineUsers = "";
+                    Users.Add(userName);
+
+                    foreach (var u in Users)
+                    {
+                        onlineUsers += u + "\n";
+                    }
+
+                    // Creates a string that populates the text box
+                    mainWindow.PlayersTextBox.Text = onlineUsers;
+                });
+
+            }
+            // Take players offline
+            else if (outInfo.Data.Contains("lost connection: Disconnected"))
+            {
+                string userName = outInfo.Data[32..^30].Trim();
+
+                // Invoke the UI thread 
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+
+                    // Removes the user who has left
+                    string onlineUsers = "";
+
+                    Users.RemoveAll(u => u == userName);
+
+                    foreach (var u in Users)
+                    {
+                        onlineUsers += u + "\n";
+                    }
+
+                    // Creates a string that populates the text box
+                    mainWindow.PlayersTextBox.Text = onlineUsers;
+                });
+            }
+            else if (outInfo.Data.Contains("left the game"))
+            {
+                string userName = outInfo.Data[32..^13].Trim();
+
+                // Invoke the UI thread 
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+
+                    // Removes the user who has left
+                    string onlineUsers = "";
+
+                    Users.RemoveAll(u => u == userName);
+
+                    foreach (var u in Users)
+                    {
+                        onlineUsers += u + "\n";
+                    }
+
+                    // Creates a string that populates the text box
+                    mainWindow.PlayersTextBox.Text = onlineUsers;
+                });
+            }
+
+            if (Application.Current != null)
+            {
+                // Invoke the UI thread 
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+
+                    mainWindow.InfoTextBox.Text += outInfo.Data + "\n";
+                });
+            }
+        }
+
+
+        /// <summary>
+        /// LEGACY
+        /// Secret commands for danny to modify server without being server host.
+        /// Danny has been give the option to use ALL commands. 
+        /// </summary>
+        /// <param name="inCommand"></param>
+        /// <returns></returns>
+        private string DauttuCommands(string inCommand)
+        {
+            //switch (inCommand)
+            //{
+            //    case @"\wish i could fly":
+            //        return "/gamemode creative Dauttu";
+            //    case @"\wish i could fight":
+            //        return "/gamemode survival Dauttu";
+            //    case @"\its lagging":
+            //        return "/reload";
+            //    case @"\too scary":
+            //        return "/difficulty peaceful";
+            //    case @"\rainraingoaway":
+            //        return "/weather clear";
+            //    case @"\sun":
+            //        return "/time set day";
+            //    case @"\moon":
+            //        return "/time set night";           
+            //    default:
+            //        break;
+
+            //}
+            return inCommand.Remove(0, 1);
+        }
+
+        /// <summary>
+        /// Displays process information
+        /// </summary>
+        private void ProcStats()
+        {
+            long memoryUsage = 0;
+
+            string cpuUsage = "";
+
+            // New thread to constantly update with RAM and CPU usage
+            Thread t = new Thread(() =>
+            {
+                while (1 < 2)
+                {   // Gets the server process
+                    foreach (var process in Process.GetProcessesByName("java"))
+                    {
+                        // Calculates the CPU usages
+                        var startTime = DateTime.Now;
+                        var startCpuUsage = process.TotalProcessorTime;
+
+                        Thread.Sleep(500);
+
+                        var endTime = DateTime.Now;
+                        var endCpuUsage = process.TotalProcessorTime;
+
+                        var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
+                        var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+
+                        var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+
+                        // Gets the RAM usage
+                        memoryUsage = process.WorkingSet64 / (1024 * 1024);
+                        cpuUsage = string.Format("{0:0.0}", cpuUsageTotal * 100);
+                    }
+
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+
+                        mainWindow.RamStatsTextBlock.Text = memoryUsage + " MB";
+                        mainWindow.cpuStatsTextBlock.Text = cpuUsage + "%";
+                    });
+                }
+
+            });
+
+            t.Start();
+        }
+    }
+}
---
+++ MS_GUI/MainWindow.xaml.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+
+namespace MS_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // java -Xmx3072M -Xms3072M -jar serverjar nogui
+
+        // Create an instance of the command class
+        Command Command = new Command();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            HideMidCom();
+            HideStartUp();
+        }
+
+        /// <summary>
+        /// Lauches the GUI and CMD
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void GoButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Make sure RAM text is not empty
+            if (RAMCB.Text != "")
+            {
+                string args = "java -Xmx" + RAMCB.Text + " -Xms" + RAMCB.Text + " -jar server.jar nogui";
+
+                // Hide controls
+                RAMCB.IsEnabled = false;
+                RAMCB.Visibility = Visibility.Hidden;
+                StartButton.IsEnabled = false;
+                StartButton.Visibility = Visibility.Hidden;
+                StartUpLabel.Visibility = Visibility.Hidden;
+                ShowGUI();
+                ShowMidCom();
+
+                // Start the server
+                await Command.InitialiseCMD(args);
+            }
+            else
+            {
+                MessageBox.Show("Please select RAM amount!", "Warning!", MessageBoxButton.OK);
+            }
+        }
+
+        /// <summary>
+        /// Fix this. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to close?", "Close", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    foreach (var process in Process.GetProcessesByName("java"))
+                    {
+                        // Kills the server and allows everything else to close properly
+                        process.Kill();
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            else
+                // Don't close if the user says No or Cancel
+                e.Cancel = true;
+        }
+
+        /// <summary>
+        /// Sends the Say command to the server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SayTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                // Create the command
+                string outCommand = "/say " + SayTextBox.Text;
+
+                Command.SendCommand(outCommand);
+
+                // Reset the text
+                SayTextBox.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// New keydown for the command text box for regular commands
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (ComTextBox.Text != "")
+                    Command.SendCommand(ComTextBox.Text);
+
+                ComTextBox.Text = "";
+            }
+        }
+
+        #region Events
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void InfoTextBox_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+
+        private void InfoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            InfoTextBox.ScrollToEnd();
+        }
+
+        private void DiffPeaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/difficulty peaceful");
+        }
+
+        private void DiffEasy_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/difficulty easy");
+        }
+
+        private void DiffNormButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/difficulty normal");
+        }
+
+        private void DiffHard_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/difficulty hard");
+        }
+
+        private void WeaClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/weather clear");
+        }
+
+        private void WeaRainButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/weather rain");
+        }
+
+        private void WeaThunButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/weather thunder");
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/save-all");
+        }
+
+        private void ReloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/reload");
+        }
+
+        /// <summary>
+        /// Kicks the selected player
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void KickButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/kick " + SelectedUserTextBlock.Text.Trim());
+
+            ShowMidCom();
+
+            PlayerDoneButton.Visibility = Visibility.Hidden;
+            PlayerBorder.Visibility = Visibility.Hidden;
+            KickButton.Visibility = Visibility.Hidden;
+            BanButton.Visibility = Visibility.Hidden;
+            Gamemode.Visibility = Visibility.Hidden;
+            GMCreative.Visibility = Visibility.Hidden;
+            GMLabel.Visibility = Visibility.Hidden;
+            GMSurvival.Visibility = Visibility.Hidden;
+            SelectedUserTextBlock.Text = "";
+            SelectedUserLabel.Visibility = Visibility.Hidden;
+            FocusLabel.Focus();
+        }
+
+        private void PlayersTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Focuses on the selected player
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PlayersTextBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            int charIndex = PlayersTextBox.GetCharacterIndexFromPoint(e.GetPosition(PlayersTextBox), true);
+
+            int getLine = PlayersTextBox.GetLineIndexFromCharacterIndex(charIndex);
+
+            string user = PlayersTextBox.GetLineText(getLine);
+
+            // TODO: Make sure this picks up all characters
+            SelectedUserTextBlock.Text = user;
+
+            if (SelectedUserTextBlock.Text != "")
+            {
+                HideMidCom();
+
+                SelectedUserLabel.Visibility = Visibility.Visible;
+
+                PlayerDoneButton.Visibility = Visibility.Visible;
+                PlayerBorder.Visibility = Visibility.Visible;
+                KickButton.Visibility = Visibility.Visible;
+                BanButton.Visibility = Visibility.Visible;
+                Gamemode.Visibility = Visibility.Visible;
+                GMCreative.Visibility = Visibility.Visible;
+                GMLabel.Visibility = Visibility.Visible;
+                GMSurvival.Visibility = Visibility.Visible;
+                FocusLabel.Focus();
+            }
+            else
+            {
+                ShowMidCom();
+
+                PlayerDoneButton.Visibility = Visibility.Hidden;
+                PlayerBorder.Visibility = Visibility.Hidden;
+                KickButton.Visibility = Visibility.Hidden;
+                BanButton.Visibility = Visibility.Hidden;
+                Gamemode.Visibility = Visibility.Hidden;
+                GMCreative.Visibility = Visibility.Hidden;
+                GMLabel.Visibility = Visibility.Hidden;
+                GMSurvival.Visibility = Visibility.Hidden;
+                SelectedUserTextBlock.Text = "";
+                SelectedUserLabel.Visibility = Visibility.Hidden;
+                FocusLabel.Focus();
+            }
+        }
+
+        /// <summary>
+        /// Hides the middle commands to show player options
+        /// </summary>
+        private void HideMidCom()
+        {
+            DiffBorder.Visibility = Visibility.Hidden;
+            DiffEasy.Visibility = Visibility.Hidden;
+            DiffHard.Visibility = Visibility.Hidden;
+            DifficultyLabel.Visibility = Visibility.Hidden;
+            DiffNormButton.Visibility = Visibility.Hidden;
+            DiffPeaceButton.Visibility = Visibility.Hidden;
+            WeaBorder.Visibility = Visibility.Hidden;
+            WeaClearButton.Visibility = Visibility.Hidden;
+            Weather.Visibility = Visibility.Hidden;
+            WeaThunButton.Visibility = Visibility.Hidden;
+            WeaRainButton.Visibility = Visibility.Hidden;
+            TimeBorder.Visibility = Visibility.Hidden;
+            TimeLabel.Visibility = Visibility.Hidden;
+            TimeDay.Visibility = Visibility.Hidden;
+            TimeNight.Visibility = Visibility.Hidden;
+            GenLabel.Visibility = Visibility.Hidden;
+            ProcStatsLabel.Visibility = Visibility.Hidden;
+            RamLabelBlock.Visibility = Visibility.Hidden;
+            RamStatsLabel.Visibility = Visibility.Hidden;
+            CpuStatsLabel.Visibility = Visibility.Hidden;
+            CpuLabelBlock.Visibility = Visibility.Hidden;
+        }
+
+        /// <summary>
+        /// Hide all GUI elements for start up
+        /// </summary>
+        private void HideStartUp()
+        {
+            PlayersLabel.Visibility = Visibility.Hidden;
+            StopButton.Visibility = Visibility.Hidden;
+            SaveButton.Visibility = Visibility.Hidden;
+            ReloadButton.Visibility = Visibility.Hidden;
+            SayTextBox.Visibility = Visibility.Hidden;
+            SayLabel.Visibility = Visibility.Hidden;
+            PlayersLabel.Visibility = Visibility.Hidden;
+            PlayersTextBox.Visibility = Visibility.Hidden;
+            InfoTextBox.Visibility = Visibility.Hidden;
+            LogLabel.Visibility = Visibility.Hidden;
+            ComTextBox.Visibility = Visibility.Hidden;
+        }
+
+        /// <summary>
+        /// Show all GUI elements after start up
+        /// </summary>
+        private void ShowGUI()
+        {
+            PlayersLabel.Visibility = Visibility.Visible;
+            StopButton.Visibility = Visibility.Visible;
+            SaveButton.Visibility = Visibility.Visible;
+            ReloadButton.Visibility = Visibility.Visible;
+            SayTextBox.Visibility = Visibility.Visible;
+            SayLabel.Visibility = Visibility.Visible;
+            PlayersLabel.Visibility = Visibility.Visible;
+            PlayersTextBox.Visibility = Visibility.Visible;
+            InfoTextBox.Visibility = Visibility.Visible;
+            LogLabel.Visibility = Visibility.Visible;
+            ComTextBox.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Shows the middle commands
+        /// </summary>
+        private void ShowMidCom()
+        {
+            DiffBorder.Visibility = Visibility.Visible;
+            DiffEasy.Visibility = Visibility.Visible;
+            DiffHard.Visibility = Visibility.Visible;
+            DifficultyLabel.Visibility = Visibility.Visible;
+            DiffNormButton.Visibility = Visibility.Visible;
+            DiffPeaceButton.Visibility = Visibility.Visible;
+            WeaBorder.Visibility = Visibility.Visible;
+            WeaClearButton.Visibility = Visibility.Visible;
+            Weather.Visibility = Visibility.Visible;
+            WeaThunButton.Visibility = Visibility.Visible;
+            WeaRainButton.Visibility = Visibility.Visible;
+            TimeBorder.Visibility = Visibility.Visible;
+            TimeLabel.Visibility = Visibility.Visible;
+            TimeDay.Visibility = Visibility.Visible;
+            TimeNight.Visibility = Visibility.Visible;
+            GenLabel.Visibility = Visibility.Visible;
+            ProcStatsLabel.Visibility = Visibility.Visible;
+            RamLabelBlock.Visibility = Visibility.Visible;
+            RamStatsLabel.Visibility = Visibility.Visible;
+            CpuStatsLabel.Visibility = Visibility.Visible;
+            CpuLabelBlock.Visibility = Visibility.Visible;
+
+        }
+
+        /// <summary>
+        /// Bans the selected player
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BanButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/ban " + SelectedUserTextBlock.Text.Trim());
+
+            ShowMidCom();
+
+            PlayerDoneButton.Visibility = Visibility.Hidden;
+            PlayerBorder.Visibility = Visibility.Hidden;
+            KickButton.Visibility = Visibility.Hidden;
+            BanButton.Visibility = Visibility.Hidden;
+            Gamemode.Visibility = Visibility.Hidden;
+            GMCreative.Visibility = Visibility.Hidden;
+            GMLabel.Visibility = Visibility.Hidden;
+            GMSurvival.Visibility = Visibility.Hidden;
+            SelectedUserTextBlock.Text = "";
+            SelectedUserLabel.Visibility = Visibility.Hidden;
+            FocusLabel.Focus();
+        }
+
+        /// <summary>
+        /// Changes selected players gamemode to creative
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GMCreative_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/gamemode creative " + SelectedUserTextBlock.Text.Trim());
+        }
+
+        /// <summary>
+        /// Changes selected players gamemode to survival
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GMSurvival_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/gamemode survival " + SelectedUserTextBlock.Text.Trim());
+        }
+
+        /// <summary>
+        /// Shows the middle commands after player options have finished
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PlayerDoneButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerDoneButton.Visibility = Visibility.Hidden;
+            PlayerBorder.Visibility = Visibility.Hidden;
+            KickButton.Visibility = Visibility.Hidden;
+            BanButton.Visibility = Visibility.Hidden;
+            Gamemode.Visibility = Visibility.Hidden;
+            GMCreative.Visibility = Visibility.Hidden;
+            GMLabel.Visibility = Visibility.Hidden;
+            GMSurvival.Visibility = Visibility.Hidden;
+            SelectedUserTextBlock.Text = "";
+            SelectedUserLabel.Visibility = Visibility.Hidden;
+            ShowMidCom();
+
+            FocusLabel.Focus();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/stop");
+        }
+
+        private void TimeDay_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/time set day");
+        }
+
+        private void TimeNight_Click(object sender, RoutedEventArgs e)
+        {
+            Command.SendCommand("/time set night");
+        }
+        #endregion
+    }
+}
